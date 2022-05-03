3f62c2de2c9316200b2d6d7d7daf03ad93374e2a
Jéssica Mota <80103910+JessicaMotta@users.noreply.github.com>
2022-05-03 19:54:11 -0300

    Add files via upload
    aula 03/05

--- index.js
+++ index.js
@@ -1,6 +1,27 @@
-console.log("Projeto Node");
-<<<<<<< HEAD
-console.log("teste");
-=======
-console.log("My name is Jesgan");
->>>>>>> a422f6378929be18e9dff553bf38c99bf47aecbb
+/*const { response } = require('express');
+const{createServer}=require('http');
+let server = createServer((request,response) => {
+    response.writeHead(200,{"Content_Type":"text/html"});
+    response.write('<h1> Olá mundo </h1><p>Projeto</p>');
+    response.end
+});
+server.listen(8000);
+console.log("Projeto Iniciado");
+*/
+let express = require('express');
+let app = express();
+app.get('/',function(req,res){
+    res.send('ROTA GET')
+});
+
+app.get('/apresentar',function(req,res){
+    /*res.send('rota 1');
+    resquisição pegando uma consulta atraves de uma variavel = nome*/
+    let X = req.query.nome;
+    res.send('Olá ' + X)
+    /*http://localhost:3000/apresentar?nome=Jessica
+    se quiser adicionar mais valores separar por interrogação */
+});
+app.listen(3000,function(){
+    console.log("Projeto iniciado na porta 3000");
+});
