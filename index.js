bc6b32e51ac7ad268de0219fe3d992bbe9bdc56f
Jéssica Mota <80103910+JessicaMotta@users.noreply.github.com>
2022-05-03 21:38:40 -0300

    atualização 03-05

--- index.js
+++ index.js
@@ -17,13 +17,10 @@ app.get('/',function(req,res){
 app.get('/apresentar',function(req,res){
     /*res.send('rota 1');
     resquisição pegando uma consulta atraves de uma variavel = nome*/
-    let param1 = req.query.nome;
     let param2 = req.query.n1;
     let param3 = req.query.n2;
     let soma = parseInt(param2) + parseInt(param3);
-    res.send('Olá ' + param1 + ' - ' + param2 + param3);
-    /*http://localhost:3000/apresentar?nome=Jessica
-    se quiser adicionar mais valores separar por interrogação */
+    res.send("Resultado " + soma);
 });
 app.listen(3000,function(){
     console.log("Projeto iniciado na porta 3000");
